332f9b0fd7c9b4a524dc08ede4cbdfece4d98e3a
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-03-10 21:10:41 +0530

    Time: 10 ms (38.43%) | Memory: 19.3 MB (60.52%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -0,0 +1,24 @@
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        notbyk=tickets[k]
+        currque= deque(range(len(tickets)))
+       
+        time=0
+        while notbyk!=0:
+            if currque:
+                x= currque.popleft()
+                tickets[x]=tickets[x]-1
+                time=time+1
+                if x==k:
+                    notbyk=notbyk-1
+                if tickets[x]!=0:
+                    currque.append(x)
+        return time
+            
+            
+        
+                    
+
+
+        
