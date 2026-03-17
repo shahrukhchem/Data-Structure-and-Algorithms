974815e8b5e034a32e92699555804b3cc0d07425
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-03-18 02:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (50.19%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -7,6 +7,7 @@
 from collections import deque
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        '''
         if not root:
             return []
         tree=deque([(root,1)])
@@ -26,7 +27,30 @@ class Solution:
         for i in res:
             fres.append(max(res[i]))
         return fres
+        '''
 
+        if not root:
+            return []
+        
+        queue = deque([root])
+        result = []
+        
+        while queue:
+            level_size = len(queue)
+            level_max = float('-inf')
+            
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_max = max(level_max, node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            result.append(level_max)
+        
+        return result
 
 
         
