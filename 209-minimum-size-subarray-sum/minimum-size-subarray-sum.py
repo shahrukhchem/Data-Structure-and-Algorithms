9e09658ac23363671849c3bf4098195706b87e17
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-03-21 01:41:29 +0530

    Time: 14 ms (21.57%) | Memory: 30.7 MB (13.41%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -15,14 +15,6 @@ class Solution:
                 r=r+1
                 if r<n:
                     currsum=currsum+nums[r]
-            '''
-            elif  r==n-1:
-                currsum=currsum-nums[l]
-                l=l+1
-            elif currsum<target:
-                r=r+1
-                currsum=currsum+nums[r]
-            '''
         if minsize==float('inf'):
             return 0
         return minsize
