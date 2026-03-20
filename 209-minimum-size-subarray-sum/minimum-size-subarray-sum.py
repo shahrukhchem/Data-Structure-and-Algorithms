6babf637c97f152e690dec8298f56f42e539976e
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-03-21 01:41:06 +0530

    Time: 16 ms (13.89%) | Memory: 30.5 MB (63.58%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,18 +5,24 @@ class Solution:
         n=len(nums)
         minsize=float('inf')
         currsum=nums[l]
-        while l<n:
+        while r<n:
             indosize=r-l+1
             if currsum>=target:
                 minsize=min(minsize,indosize)
                 currsum=currsum-nums[l]
                 l=l+1
+            else:
+                r=r+1
+                if r<n:
+                    currsum=currsum+nums[r]
+            '''
             elif  r==n-1:
                 currsum=currsum-nums[l]
                 l=l+1
             elif currsum<target:
                 r=r+1
                 currsum=currsum+nums[r]
+            '''
         if minsize==float('inf'):
             return 0
         return minsize
