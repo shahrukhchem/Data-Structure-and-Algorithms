00e638e4c4ba2cbb0ba16e980c6b8daf99fdebd4
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-03-25 01:12:03 +0530

    Time: 3 ms (3.92%) | Memory: 19.6 MB (28.94%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        #unrotate
+        n=len(nums)
+        i=0
+        found=False
+        if n>1:
+            for i in range(1,n):
+                if nums[i]<nums[i-1]:
+                    found = True
+                    break
+        
+        if not found:
+            i = 0
+        unrotatenums=nums[i:]+nums[:i]
+        print(unrotatenums)
+        #binarysearch 
+        L=0
+        R=n-1
+        while L<=R:
+            M=L+((R-L)//2)
+            
+            if unrotatenums[M]==target:
+                return (M + i) % n
+            elif target<unrotatenums[M]:
+                R=M-1
+            else:
+                L=M+1
+        return -1
