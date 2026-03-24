4081249774c7b9d6aef40ba616b46fe106f3a275
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-03-24 16:18:10 +0530

    Time: 319 ms (5.10%) | Memory: 21 MB (31.63%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        n=len(arr)
+        l=0
+        r=0
+        indosize=r-l+1
+        currindo=[]
+        totaldisfromx=0
+        maxdist=float('inf')
+        for l in range(0,n-k+1):
+            while r-l+1<k+1:
+                currindo.append(arr[r])
+                totaldisfromx=totaldisfromx+abs(arr[r]-x)
+                r=r+1
+            if totaldisfromx<maxdist:
+                maxdist=totaldisfromx
+                res=currindo.copy()
+            totaldisfromx=totaldisfromx-abs(currindo[0]-x)
+            currindo.pop(0)
+        return res
+            
