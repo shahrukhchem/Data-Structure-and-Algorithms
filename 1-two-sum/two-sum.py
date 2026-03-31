52763ec09e2e08304fa9eb00ab3b5728d3ac0054
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-04-01 01:47:57 +0530

    Time: 7 ms (34.65%) | Memory: 21.5 MB (6.97%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -10,14 +10,29 @@ class Solution:
             else:
                 c[nums[i]]=[i]
         r=n-1
+        for i in range(0,n):
+            fa=i
+            currtarget=target-nums[i]
+            if currtarget==nums[i]:
+                if len(c[currtarget])==1:
+                    continue
+
+            print(currtarget)
+            if currtarget in c:
+
+                if currtarget==nums[i]:
+                    return [fa,c[nums[i]][1]]
+                else:
+                    return [fa,c[currtarget][0]]
+        '''
         while l<r:
             if sortednums[l]+sortednums[r]==target:
                 if sortednums[l]==sortednums[r]:
                     return [c[sortednums[l]][0],c[sortednums[r]][1]]
                 else:
                     return [c[sortednums[l]][0],c[sortednums[r]][0]]
-                    
             elif sortednums[l]+sortednums[r]>target:
                 r=r-1
             else:
                 l=l+1
+        '''
