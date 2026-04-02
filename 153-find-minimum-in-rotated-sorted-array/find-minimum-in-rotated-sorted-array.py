08425d769e3f97798ea233e64b499073e1de3a55
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-04-03 01:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (79.55%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -6,16 +6,13 @@ class Solution:
         r=n-1
         while l<=r:
             m=l+((r-l)//2)
-            print(l,m,r,ans)
             if nums[l]<=nums[r]:
                 ans=min(ans,nums[l])
                 return ans
             elif nums[l]<=nums[m]:
-                print('onside',ans,nums[m])
                 ans=min(ans,nums[l])
                 l=m+1
             elif nums[r]>=nums[m]:
-                print('inside',ans,nums[m])
                 ans=min(ans,nums[m])
                 r=m-1
         return ans
