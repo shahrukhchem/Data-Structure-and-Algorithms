ae807dc406a6ac644b5a4d14d532ee484aba5992
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-04-05 02:29:27 +0530

    Time: 3339 ms (5.04%) | Memory: 24.8 MB (18.80%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n=len(citations)
+        l=0
+        r=n
+        ans=0
+        def ishpossible(h):
+            count=0
+            for i in citations:
+                if i>=h:
+                    count+=1
+            return count>=h
+
+        for h in range(0,r+1):
+            print(h,ishpossible(h))
+            if not ishpossible(h):
+                return h-1
+        return n
