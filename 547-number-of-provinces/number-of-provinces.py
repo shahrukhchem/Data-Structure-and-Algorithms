51ec201ecad91b665624019ec9eb4c00e7868ff7
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-04-06 01:43:08 +0530

    Time: 7 ms (50.82%) | Memory: 21 MB (40.44%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n=len(isConnected)
+        graph=defaultdict(list)
+        for ro in range(0,n):
+            for col in range(0,len(isConnected[ro])):
+                if isConnected[ro][col]==1:
+                  graph[ro+1].append(col+1)
+        
+        def dfsrecur(node):
+            for neinode in graph[node]:
+                if neinode not in seen:
+                    seen.add(neinode)
+                    dfsrecur(neinode)
+        seen=set()
+        res=[]
+        for n  in range(1,n+1):
+            dfsrecur(n)
+            res.append(tuple(seen))
+        res=set(res)
+        return len(res)
+
+
+        
+        
