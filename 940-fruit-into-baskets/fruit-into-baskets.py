e19e4fff12e196a729f3ef716afbbfbb6a27e0ac
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-06-26 23:16:47 +0530

    Time: 209 ms (18.69%) | Memory: 25.9 MB (63.29%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,26 +1,24 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        l=0
+        left=0
+        fruitset={}
         n=len(fruits)
-        r=0
-        currfruits={}
-        maxfruit=0
-        for l in range(0,n):
-            while  r<n:
-                fr=fruits[r]
-                currfruits[fr] = currfruits.get(fr, 0) + 1
-                if len(currfruits)>=3:
-                    currfruits[fr] = currfruits[fr] - 1
-                    if currfruits[fr]==0:
-                        currfruits.pop(fr)
-                    break
-                maxfruit=max(maxfruit,r-l+1)
-                r=r+1
-            fl=fruits[l]
-            currfruits[fl] = currfruits.get(fl, 0) - 1
-            if currfruits[fl]==0:
-                currfruits.pop(fl)
-        return maxfruit
+        ans=0
+        for right in range(n):
             
+            
+            if fruits[right] in fruitset:
+                fruitset[fruits[right]]+=1
+            else:
+                fruitset[fruits[right]]=1
+           
+            while len(fruitset)>=3:
+                fruitset[fruits[left]]-=1
+                if fruitset[fruits[left]] == 0:
+                    del fruitset[fruits[left]]
+                left=left+1
+            
+            ans=max(ans,right-left+1)
+        return ans
 
         
