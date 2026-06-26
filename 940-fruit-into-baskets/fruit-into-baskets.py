e8e4af2a211a3280186f2d114b7b381f94d83059
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-06-26 23:17:06 +0530

    Time: 190 ms (49.39%) | Memory: 26 MB (34.44%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -5,19 +5,15 @@ class Solution:
         n=len(fruits)
         ans=0
         for right in range(n):
-            
-            
             if fruits[right] in fruitset:
                 fruitset[fruits[right]]+=1
             else:
                 fruitset[fruits[right]]=1
-           
             while len(fruitset)>=3:
                 fruitset[fruits[left]]-=1
                 if fruitset[fruits[left]] == 0:
                     del fruitset[fruits[left]]
                 left=left+1
-            
             ans=max(ans,right-left+1)
         return ans
 
