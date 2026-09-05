351b933e58acdf1ebc7aeae9f0018bb4428952da
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-09-05 23:17:50 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.49%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        found = set()
+        stack=[([],nums)]
+        totalsol=0
+        res=[]
+        while stack:
+            currentchoice,optionsavailable=stack.pop()
+            k=tuple(sorted(currentchoice))
+            if not optionsavailable:
+                if  k not in found:
+                    found.add(k)
+                    res.append(currentchoice)
+                continue
+            if  k not in found:
+                    found.add(k)
+                    res.append(currentchoice)
+            stack.append((currentchoice+[optionsavailable[-1]],optionsavailable[:-1]))
+            stack.append((currentchoice,optionsavailable[:-1]))
+        return res
+        
