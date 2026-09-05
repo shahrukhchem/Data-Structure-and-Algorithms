d5f0a788e682d33d7cc04118c93d94400599d837
Mohd Shahrukh <mohdshahrukh3012@gmail.com>
2026-09-05 17:10:44 +0530

    Time: 70 ms (5.04%) | Memory: 19.3 MB (91.68%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,17 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        sol,res=[],[]
-        n=len(candidates)
-        def dfsrecur(start,curr_sum,numbers):
-            if curr_sum==target:
-                res.append(numbers.copy())
-                return
-            if curr_sum>target:
-                return
-            for num in range(start,len(candidates)):
-                numbers.append(candidates[num])
-                dfsrecur(num,curr_sum + candidates[num],numbers)
-                numbers.pop()
-        dfsrecur(0,0,[])
+        found = set()
+        stack=[([],candidates)]
+        totalsol=0
+        res=[]
+        while stack:
+            currentchoice,optionsavailable=stack.pop()
+            if not optionsavailable:
+                if sum(currentchoice)==target and tuple(currentchoice) not in found:
+                                            totalsol+=1
+                                            found.add(tuple(currentchoice))
+                                            res.append(currentchoice)
+                continue
+            if sum(currentchoice)==target and tuple(currentchoice) not in found:
+                                totalsol+=1
+                                found.add(tuple(currentchoice))
+                                res.append(currentchoice)
+                                
+            if sum(currentchoice) < target:
+                stack.append((currentchoice+[optionsavailable[-1]],optionsavailable))
+            stack.append((currentchoice,optionsavailable[:-1]))
         return res
-
